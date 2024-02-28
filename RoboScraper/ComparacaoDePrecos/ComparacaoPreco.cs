bcf04a8974079d9aa5f2cd6b7c7df7bf7525590a
Larissa Lopes de Oliveira <larissa.arqueo@outlook.com>
2024-02-28 17:32:00 -0300

    Adicionar arquivos de projeto.

---
+++ RoboScraper/ComparacaoDePrecos/ComparacaoPreco.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoboScraper.ComparacaoDePrecos
+{
+    internal class ComparacaoPreco
+    {
+        public static string CompararPreco(string precoMerL, string precoMagL)
+        {
+            char[] removerCaracteres = { 'R', '$', ' ' };
+
+            double precoMagazine = Convert.ToDouble(precoMagL.Trim(removerCaracteres));
+            double precoMercado = Convert.ToDouble(precoMerL.Trim(removerCaracteres));
+
+            Console.WriteLine(precoMercado);
+            Console.WriteLine(precoMagazine);
+
+            if (precoMagazine > precoMercado)
+            {
+                return "Melhor preço: Mercado Livre";
+            }
+            else if (precoMercado > precoMagazine)
+            {
+                return "Melhor preço: Magazine Luíza";
+            }
+            else
+            {
+                return "Preços iguais";
+            }
+        }
+    }
+}
---
+++ RoboScraper/Send/SendEmail.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoboScraper.Send
+{
+
+    public class SendEmail
+    {
+        static void EnviarEmail(string nomeProdutoMercadoLivre, string precoProdutoMercadoLivre, string nomeProdutoMagazineLuiza, string precoProdutoMagazineLuiza)
+        {
+            // Configurações do servidor SMTP do Gmail
+            string smtpServer = "smtp-mail.outlook.com"; // Servidor SMTP do Gmail
+            int porta = 587; // Porta SMTP do Gmail para TLS/STARTTLS
+            string remetente = "wallacemaximus@hotmail.com"; // Seu endereço de e-mail do Gmail
+            string senha = "teste"; // Sua senha do Gmail
+
+            // Configurar cliente SMTP
+            using (SmtpClient client = new SmtpClient(smtpServer, porta))
+            {
+                client.UseDefaultCredentials = false;
+                client.Credentials = new NetworkCredential(remetente, senha);
+                client.EnableSsl = true; // Habilitar SSL/TLS
+
+                // Construir mensagem de e-mail
+                MailMessage mensagem = new MailMessage(remetente, "wallace@docente.senai.br")
+                {
+                    Subject = "Resultado da comparação de preços",
+                    Body = $"Produto do Mercado Livre: {nomeProdutoMercadoLivre} - Preço: {precoProdutoMercadoLivre}\n" +
+                           $"Produto do Magazine Luiza: {nomeProdutoMagazineLuiza} - Preço: {precoProdutoMagazineLuiza}\n"
+
+                };
+
+                // Enviar e-mail
+                client.Send(mensagem);
+
+
+                Console.WriteLine(nomeProdutoMercadoLivre);
+                Console.WriteLine(precoProdutoMercadoLivre);
+                Console.WriteLine(nomeProdutoMagazineLuiza);
+                Console.WriteLine(precoProdutoMagazineLuiza);
+
+            }
+        }
+    }
+}
