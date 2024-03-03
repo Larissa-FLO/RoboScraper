b61d3a7646bcafb66ffcde1a5e1474c7793063a6
Larissa Lopes de Oliveira <152396589+Larissa-FLO@users.noreply.github.com>
2024-03-02 23:03:30 -0300

    correção da connection string

--- RoboScraper/Program.cs
+++ RoboScraper/Program.cs
@@ -20,7 +20,10 @@ public class LogContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
-        optionsBuilder.UseSqlServer("Server=PC03LAB2512\\SENAI;Database=WebScrapingDb2;User Id=sa;Password=senai.123;"); // Substitua "YourConnectionString" pela sua string de conexão
+        optionsBuilder.UseSqlServer(@"Data Source=SQL9001.site4now.net;" +
+                                    "Initial Catalog=db_aa5b20_apialmoxarifado;" +
+                                    "User id=db_aa5b20_apialmoxarifado_admin;" +
+                                    "Password=master@123");
     }
 }
 
